f580d63d135cf08df58a7665a93bf2fa5d8ca2d7
KrunalDarekar <96249206+KrunalDarekar@users.noreply.github.com>
2024-02-04 12:46:27 +0530

    Add the data fetching hooks

---
+++ src/DataHooks.jsx
@@ -0,0 +1,53 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
+const useMensClothing = () => {
+    const [mensData, setMensData] = useState(null)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products/category/men's clothing")
+            .then((res) => {
+                if (res.status >= 400) {
+                    throw new Error("server error")
+                }
+                return res.json()
+            })
+            .then((res) => setMensData(res))
+            .catch((error) => setError(error))
+            .finally(() => setLoading(false))
+
+    }, [])
+
+    return { mensData, error, loading}
+
+}
+
+const useWomensClothing = () => {
+    const [womensData, setWomensData] = useState(null)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products/category/women's clothing")
+            .then((res) => {
+                if (res.status >= 400) {
+                    throw new Error("server error")
+                }
+                return res.json()
+            })
+            .then((res) => setWomensData(res))
+            .catch((error) => setError(error))
+            .finally(() => setLoading(false))
+
+    }, [])
+
+    return { womensData, error, loading}
+
+}
+
+export {
+    useMensClothing,
+    useWomensClothing
+}
