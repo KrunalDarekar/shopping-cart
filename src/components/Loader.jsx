203ad8c135a77c1ee3e83aa6065dd59f38300009
KrunalDarekar <96249206+KrunalDarekar@users.noreply.github.com>
2024-02-09 16:40:00 +0530

    Add the loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+const Loader = () => {
+
+    return (
+        <div className="w-full h-96 flex justify-center items-center mt-28">
+            <div className="w-8 h-8 border-4 border-purple-700 border-t-purple-200 rounded-full animate-spin">
+
+            </div>
+        </div>
+    )
+}
+
+export default Loader
